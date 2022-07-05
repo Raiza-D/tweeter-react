da0d082db9309dca4c0256daba0f9da55907f3f4
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-05 14:37:50 -0700

    Remove hard-coded values for name and profile pic. Declare variables and use them in place of hard-coded values.

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 function Profile() {
+  const firstName = "Amy"
+  const lastName = "Mansell"
+  const avatar = "./profile-hex.png"
+
   return (
     <aside>
       <div class="profile">
-        <img class="profile__image" src="./profile-hex.png" alt="avatar" />
+        <img class="profile__image" src={avatar} alt="avatar" />
       </div>
       <br />
       <div class="profile__name">
-        <h2><span class="profile--bold">Amy</span> Mansell</h2>
+        <h2><span class="profile--bold">{firstName}</span> {lastName}</h2>
       </div>
     </aside>
   );
