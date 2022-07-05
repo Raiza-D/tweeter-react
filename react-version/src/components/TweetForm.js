db52999364e2a77814fd7587ed189b5d0c7d9727
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-05 16:08:03 -0700

    Add event handler to submit button, prevent the default behaviour of elem usint preventDefault() method to input elem.

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -7,7 +7,12 @@ function TweetForm() {
     <section class="newtweet">
       <form method="post" action="/tweets" class="newtweet__form">
         <textarea class="form__textarea" name="text" placeholder={tweetFormPlaceholder}></textarea>
-        <input type="submit" value="Tweet" class="form__input"/>
+        <input type="submit" value="Tweet" class="form__input"
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+        
+        />
         <span class="form__counter">140</span>
       </form>
     </section>
