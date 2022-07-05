8ac21516fff347da8afbc55ce1ae5f95e05a9184
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-04 21:32:09 -0700

    Import components .js files. Edit App function by adding the components in JSX format inside the return statement. Import the App.css file.

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import TweetForm from './components/TweetForm';
+import TweetList from './components/TweetList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Profile />
+      <TweetForm />
+      <TweetList />
     </div>
   );
-}
+};
 
 export default App;
